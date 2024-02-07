30cca7b0aacd8a204405f3575fefcf16f7efef9c
4FViP <152526348+4FViP@users.noreply.github.com>
2024-02-07 15:48:56 +0200

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5892748077:AAFvofY_-6du_89K56vSp0H9ZWxAAtrWrvw")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5901305358:AAG_DA1t3CXnakCe8BAEsNWGG5uDU8MtpJk")
 
-    APP_ID = int(os.environ.get("APP_ID", 10920209))
+    APP_ID = int(os.environ.get("APP_ID", 11750778))
 
-    API_HASH = os.environ.get("API_HASH", "16e40a84635f770e382417b71be6f268")
+    API_HASH = os.environ.get("API_HASH", "d0352df3ddb5e00bcf16b55dae071b52")
